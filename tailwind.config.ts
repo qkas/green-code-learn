385fa4c6f78e48503ed92e89ed739be547a6c15c
Niroasd <123634396+Niroasd@users.noreply.github.com>
2025-02-10 15:42:18 +0200

    Assigned color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        accent: "var(--accent)",
+        highlight: "var(--highlight)",
       },
     },
   },
