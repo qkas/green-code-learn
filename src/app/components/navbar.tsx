666b78dced786b47d126a0bf5e93de388d598ffa
qkas <kassuvaa@gmail.com>
2025-02-10 20:06:00 +0200

    Added hover effects for navbar buttons

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,18 +7,22 @@ export default function Navbar() {
   return (
     <nav className="flex items-center justify-around px-6 py-4 bg-transparent">
       <Link href="/">
-        <div className="flex items-center">
+        <div className="relative flex items-center group hover:scale-105">
           <Image
             src="/leaves-light.png"
             alt="CodeGreen Logo"
             width={60}
             height={60}
           />
-          <h1 className="text-3xl font-bold rounded-lg text-[var(--foreground)]">Codegreen</h1>
+          <h1 className="text-3xl pr-3 font-bold rounded-lg text-[var(--foreground)] transition">
+            Codegreen
+          </h1>
+          {/* underline effect */}
+          <div className="absolute bottom-[4px] left-1 w-full h-[2px] bg-[var(--foreground)] scale-x-0 origin-center transition-transform duration-75 ease-in-out group-hover:scale-x-100"></div>
         </div>
       </Link>
       <Link href="/login">
-        <button className="px-8 py-2 font-bold rounded-lg bg-[var(--foreground)] text-[var(--accent)] hover:bg-[var(--accent)] hover:text-[var(--foreground)] transition">
+        <button className="px-10 py-2 font-bold rounded-lg bg-[var(--foreground)] text-[var(--background)] hover:border-[var(--foreground)] hover:scale-105 border-2 hover:bg-[var(--background)] hover:text-[var(--foreground)] transition">
           Get Started
         </button>
       </Link>
