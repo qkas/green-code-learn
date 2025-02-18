a1fa67b9343014e0e9f8c93ab74a2d7eedd37d77
qkas <kassuvaa@gmail.com>
2025-02-18 12:48:10 +0200

    Fixed footer's responsiveness

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,11 +4,11 @@ import { Copyright } from "@mui/icons-material";
 
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-between text-xs text-[--highlight] mt-20 p-8">
+    <footer className="flex flex-wrap items-center text-center px-2 sm:px-10 justify-center sm:justify-between text-xs text-[--highlight] gap-4 mt-8 py-8 font-[family-name:var(--font-geist-sans)]">
       <span><Copyright fontSize="inherit"/>2025 CodeGreen. All Rights Reserved</span>
-      <div>
-        <Link href="/" className="mx-5 hover:underline">Terms of Service</Link>
-        <Link href="/" className="mx-5 hover:underline">Privacy</Link>
+      <div className="flex justify-center text-center gap-4">
+        <Link href="/" className="hover:underline">Terms of Service</Link>
+        <Link href="/" className="hover:underline">Privacy</Link>
       </div>
     </footer>
   )
