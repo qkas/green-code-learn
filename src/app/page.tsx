dcf0cbd27abf5868bb39b06c1d1025da28464761
qkas <kassuvaa@gmail.com>
2025-02-20 15:38:12 +0200

    Added responsive font sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <div className="flex flex-col text-center mt-20 gap-20 items-center p-6">
 
-      <h1 className="max-w-6xl text-6xl font-bold">
+      <h1 className="max-w-6xl text-3xl sm:text-5xl lg:text-6xl font-bold">
         Green Code Certificate – Build <span className="text-[--highlight]">Sustainable</span> Software
       </h1>
 
-      <p className="text-lg max-w-5xl">
+      <p className="text-base sm:text-lg lg:text-2lg max-w-5xl">
         The Green Code Certificate is a study program for developers, aiming to teach sustainable programming practices.
         From choosing the correct frameworks to optimizing code, this course will explain all you need to know aboutwriting green code.
       </p>
@@ -17,7 +17,7 @@ export default function Home() {
       <PricingSection />
 
       <div className="flex flex-col gap-5 items-center">
-        <h2 className="text-2xl font-bold">Sign up and learn green development today!</h2>
+        <h2 className="text-xl sm:text-3xl lg:text-4xl font-bold">Sign up and learn green development today!</h2>
         <GetStartedButton />
       </div>
     </div>
