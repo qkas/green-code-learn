ad3d33b01cdc0c767eead286b8f82d9311b18937
qkas <kassuvaa@gmail.com>
2025-02-20 13:57:17 +0200

    Improved pricing section responsiveness

--- src/components/ui/pricing-card.tsx
+++ src/components/ui/pricing-card.tsx
@@ -11,8 +11,8 @@ interface PricingCardProps {
 export default function PricingCard({ title, price, features, link = "/login" }: PricingCardProps) {
   return (
     <Link href={link}>
-      <div className="px-10 flex flex-col items-center text-center group py-5 mx-5 min-h-96 font-[family-name:var(--font-geist-sans)] rounded-lg bg-[var(--background)] text-[var(--foreground)] hover:border-[var(--background)] hover:scale-105 border-2 hover:bg-[var(--foreground)] hover:text-[var(--background)] transition">
-        <div className="flex justify-between w-full items-center gap-x-4 my-5">
+      <div className="p-5 flex flex-col items-center text-center group min-h-96 max-w-80 rounded-lg border-2 border-foreground bg-background text-foreground hover:border-background hover:scale-105 hover:bg-foreground hover:text-background transition">
+        <div className="flex justify-around w-full items-center gap-x-4 my-5">
           <h3 className="font-bold text-2xl">{title}</h3>
           <h3 className="text-xl">{price}</h3>
         </div>
@@ -24,7 +24,7 @@ export default function PricingCard({ title, price, features, link = "/login" }:
             </li>
           ))}
         </ul>
-        <button className="mt-auto mb-5 py-2 w-full font-bold font-[family-name:var(--font-geist-mono)] rounded-lg bg-[var(--foreground)] text-[var(--background)] group-hover:border-[var(--foreground)] border-2 group-hover:bg-[var(--background)] group-hover:text-[var(--foreground)] hover:scale-105 transition">
+        <button className="mt-auto mb-5 py-2 w-full uppercase font-bold rounded-lg border-2 border-foreground bg-foreground text-background group-hover:border-foreground group-hover:bg-background group-hover:text-foreground hover:scale-105 transition">
           Get Started
         </button>
       </div>
--- src/components/ui/pricing-section.tsx
+++ src/components/ui/pricing-section.tsx
@@ -2,31 +2,33 @@ import PricingCard from "@/components/ui/pricing-card";
 
 export default function PricingSection() {
 	return (
-		<div className="flex justify-center mt-10">
-			<PricingCard
-				title="Trial"
-				price="FREE"
-				features={["Access to all modules"]}
-			/>
-
-			<PricingCard
-				title="Pro"
-				price="329.99€"
-				features={[
-					"Access to all modules",
-					"Access to certification"
-				]}
-			/>
-
-			<PricingCard
-				title="Enterprise"
-				price="1249.99€"
-				features={[
-					"Up to 20 premium accounts",
-					"With access to all modules",
-					"And access to certification"
-				]}
-			/>
+		<div>
+			<h2 className="text-4xl font-bold">Explore our pricing plans</h2>
+			<h3 className="text-5md mb-10 font-bold text-[--highlight]">All paid plans include a 3-day free trial</h3>
+			<div className="flex flex-wrap gap-5 justify-center">
+				<PricingCard
+					title="Trial"
+					price="FREE"
+					features={["Access to all modules"]}
+				/>
+				<PricingCard
+					title="Pro"
+					price="329.99€"
+					features={[
+						"Access to all modules",
+						"Access to certification"
+					]}
+				/>
+				<PricingCard
+					title="Enterprise"
+					price="1249.99€"
+					features={[
+						"Up to 20 premium accounts",
+						"With access to all modules",
+						"And access to certification"
+					]}
+				/>
+			</div>
 		</div>
 	);
 }
