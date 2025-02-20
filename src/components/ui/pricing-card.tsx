a448e6b2dc702259c7995cca93f6f496061a46bd
qkas <kassuvaa@gmail.com>
2025-02-20 18:42:50 +0200

    Removed excessive hover effects

--- src/components/ui/pricing-card.tsx
+++ src/components/ui/pricing-card.tsx
@@ -12,7 +12,7 @@ export default function PricingCard({ title, price, features, link = "/login" }:
   return (
     <Link
       href={link}
-      className="p-5 gap-10 md:gap-5 flex flex-col justify-center text-center min-full w-full md:w-fit group rounded-lg border-2 border-foreground bg-background text-foreground hover:border-background hover:scale-105 hover:bg-foreground hover:text-background transition"
+      className="p-5 gap-10 md:gap-5 flex flex-col justify-center text-center min-full w-full md:w-fit rounded-lg border-2 border-foreground"
     >
       <div className="flex justify-between items-center gap-5">
         <h3 className="font-bold text-2xl">{title}</h3>
@@ -26,7 +26,7 @@ export default function PricingCard({ title, price, features, link = "/login" }:
           </li>
         ))}
       </ul>
-      <button className="mt-auto py-2 w-[80%] mx-auto uppercase font-bold rounded-lg border-2 border-foreground bg-foreground text-background group-hover:border-foreground group-hover:bg-background group-hover:text-foreground hover:scale-105 transition">
+      <button className="mt-auto py-2 w-[80%] mx-auto uppercase font-bold rounded-lg border-2 border-foreground bg-foreground text-background hover:border-foreground hover:bg-background hover:text-foreground hover:scale-105 transition">
         Get Started
       </button>
     </Link>
