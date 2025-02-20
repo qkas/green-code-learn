691d1f0117af4cda810edf4b465218b55d110be1
qkas <kassuvaa@gmail.com>
2025-02-20 16:58:41 +0200

    Added more color to get started button

--- src/components/ui/get-started-button.tsx
+++ src/components/ui/get-started-button.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function GetStartedButton() {
   return (
     <Link href="/login">
-      <div className="py-2 text-center uppercase text-nowrap w-fit px-4 sm:px-8 lg:px-10 text-sm sm:text-base lg:text-lg font-bold rounded-lg border-2 border-foreground bg-foreground text-background hover:border-foreground hover:scale-105 hover:bg-background hover:text-foreground transition">
+      <div className="py-2 text-center uppercase text-nowrap w-fit px-4 sm:px-8 lg:px-10 text-sm sm:text-base lg:text-lg font-bold rounded-lg border-2 border-[--accent] bg-[--accent] text-background hover:border-[--accent] hover:scale-105 hover:bg-background hover:text-[--accent] transition">
         Get Started
       </div>
     </Link>
