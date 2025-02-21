46c80b21324cb6924742011046fdfd5604093837
qkas <kassuvaa@gmail.com>
2025-02-21 15:39:10 +0200

    Added boilerplate login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,17 @@
+export default function Login() {
+  return (
+    <div className="flex flex-col w-fit mx-auto rounded-2xl border-2 border-foreground text-center mt-20 gap-10 items-center p-6">
+      <div className="flex flex-col gap-5">
+        <h1 className="max-w-6xl text-3xl sm:text-5xl lg:text-6xl font-bold">
+          Login
+        </h1>
+        <h2>
+          Signup required for access to course material
+        </h2>
+      </div>
+      <div className="flex flex-col gap-5">
+
+      </div>
+    </div>
+  );
+}
