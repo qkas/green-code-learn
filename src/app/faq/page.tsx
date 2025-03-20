3b7e09a77bc7252f2aec549ca375d1533ded7b24
qkas <kassuvaa@gmail.com>
2025-03-20 16:45:48 +0200

    Create FAQ boilerplate

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,12 @@
+export default function Faq() {
+  return (
+    <div className="flex flex-col text-center mt-20 gap-10 items-center p-6">
+      <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold">
+        Recently asked questions
+      </h1>
+      <p className="text-base sm:text-lg lg:text-2lg max-w-5xl">
+        Coming soon...
+      </p>
+    </div>
+  )
+}
