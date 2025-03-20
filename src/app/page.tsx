5773c21eb1b8919ec37a3bff7eb67af289e44a06
qkas <kassuvaa@gmail.com>
2025-03-20 17:28:06 +0200

    Add background leaf to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import PricingSection from "@/components/ui/pricing-section";
 import GetStartedButton from "@/components/ui/get-started-button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col text-center mt-20 gap-20 items-center p-6">
-
-      <h1 className="max-w-6xl text-3xl sm:text-5xl lg:text-6xl font-bold">
+      <div className="z-0 absolute bg-[url('/leaf.png')] top-20 w-full h-80 bg-contain bg-no-repeat bg-center pointer-events-none" />
+      
+      <h1 className="z-10 max-w-6xl text-3xl sm:text-5xl lg:text-6xl font-bold">
         EcoBit Green Code Certificate – Build <span className="text-[--highlight]">Sustainable</span> Software
       </h1>
 
-      <p className="text-base sm:text-lg lg:text-2lg max-w-5xl">
+      <p className="z-10 text-base sm:text-lg lg:text-2lg max-w-5xl">
         The EcoBit Green Code Certificate is a study program for developers, aiming to teach sustainable programming practices.
         From choosing the correct frameworks to optimizing code, this course will explain all you need to know about writing green code.
       </p>
