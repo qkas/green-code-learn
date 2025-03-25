fe94b1b1898a86efa812156a63f07dae298c5584
qkas <kassuvaa@gmail.com>
2025-03-25 12:14:38 +0200

    Routing directly to course if logged in

--- src/components/ui/get-started-button.tsx
+++ src/components/ui/get-started-button.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 export default function GetStartedButton() {
+  const { data: session } = useSession();
   return (
-    <Link href="/login">
+    <Link href={session ? "/course" : "/login"}>
       <div className="py-2 text-center uppercase text-nowrap w-fit px-4 sm:px-8 lg:px-10 text-sm sm:text-base lg:text-lg font-bold rounded-lg border-2 border-[--accent] bg-[--accent] text-background hover:border-[--accent] hover:scale-105 hover:bg-background hover:text-[--accent] transition">
         Get Started
       </div>
