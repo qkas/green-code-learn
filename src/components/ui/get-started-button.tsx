4ee065ba88eaf43895a1f6f62805d440a0cce344
qkas <kassuvaa@gmail.com>
2025-04-02 14:11:39 +0300

    Add conditional rendering to GetStartedButton

--- src/components/ui/get-started-button.tsx
+++ src/components/ui/get-started-button.tsx
@@ -8,7 +8,7 @@ export default function GetStartedButton() {
   return (
     <Link href={session ? "/course" : "/login"}>
       <div className="py-2 text-center uppercase text-nowrap w-fit px-4 sm:px-8 lg:px-10 text-sm sm:text-base lg:text-lg font-bold rounded-lg border-2 border-[--accent] bg-[--accent] text-background hover:border-[--accent] hover:scale-105 hover:bg-background hover:text-[--accent] transition">
-        Get Started
+      {session ? "Start course" : "Sign up"}
       </div>
     </Link>
   )
