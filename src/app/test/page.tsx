07fffb40eca8aa2b13422463877a7ceabf28c4ca
qkas <kassuvaa@gmail.com>
2025-04-13 18:34:11 +0300

    Expand alphabet list for more options support

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -20,7 +20,7 @@ export default function TestPage() {
     isSubmitted: false,
   });
 
-  const alphabets = ['A', 'B', 'C', 'D']
+  const alphabets = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
   const handleAnswerSelect = (optionIndex: number, questionIndex: number) => {
     if (quizState.isSubmitted) return;
