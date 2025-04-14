9c9a6d56b8933b9f90a4e4f9c534a99d6a63136c
Kasperi Jaakkola <kassuvaa@gmail.com>
2025-04-14 11:38:40 +0300

    Style NextModuleLink

--- src/components/ui/NextModuleLink.tsx
+++ src/components/ui/NextModuleLink.tsx
@@ -40,9 +40,9 @@ export function NextModuleLink({ currentPath }: { currentPath: string }) {
     <div className="mt-5 flex flex-col items-end justify-end">
       <Link
         href={nextModule?.path || "/test"}
-        className={`flex py-2 text-center gap-2 items-center uppercase w-fit px-4 sm:px-8 lg:px-10 font-bold rounded-lg border-2 border-accent
+        className={`flex py-2 text-center gap-2 items-center uppercase w-fit px-4 sm:px-8 lg:px-10 text-sm sm:text-base font-bold rounded-lg border-2 border-accent
           ${!isQuizSubmitted
-            ? 'bg-gray-300 border-gray-300 cursor-not-allowed opacity-50'
+            ? 'border-foreground cursor-not-allowed opacity-50'
             : 'bg-accent text-background hover:border-accent hover:scale-105 hover:bg-background hover:text-accent'
           } transition`}
         onClick={(e) => {
